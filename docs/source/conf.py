464d0a03d2f2e5eb94371aa619347734effc5831
amirmat98 <matin.amirmahdi@gmail.com>
2025-04-27 01:15:26 +0200

    update sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,11 +9,24 @@
 project = 'Assignment_Cogar_Group_N'
 copyright = '2025, AmirMahdi Matin - Mahmoud M. A. Elderiny'
 author = 'AmirMahdi Matin - Mahmoud M. A. Elderiny'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+'sphinx.ext.autodoc',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.todo',
+'sphinx.ext.coverage',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.viewcode',
+'sphinx.ext.githubpages',
+"sphinx.ext.napoleon",
+'sphinx.ext.inheritance_diagram'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +36,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+highlight_language = 'python'
+source_suffix = '.rst'
+master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
