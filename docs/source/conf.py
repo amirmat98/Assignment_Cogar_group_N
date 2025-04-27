36552e56467eea9b85c5c2339fcebc10e7a2ef62
amirmat98 <matin.amirmahdi@gmail.com>
2025-04-27 23:24:22 +0200

    make compatible document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
 project = 'Assignment_Cogar_Group_N'
 copyright = '2025, AmirMahdi Matin - Mahmoud M. A. Elderiny'
 author = 'AmirMahdi Matin - Mahmoud M. A. Elderiny'
@@ -31,6 +35,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = ["rospy", "std_msgs", "sensor_msgs", "assignments"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -41,3 +46,13 @@ source_suffix = '.rst'
 master_doc = 'index'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_show_sourcelink = False
+
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "amirmat98",  # Username
+    "github_repo": "Assignment_Cogar_Group_N",  # Repo name
+    "github_version": "main", 
+    "conf_py_path": "/docs/source/",  # Path in the checkout to the docs root
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,8 +3,10 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Assignment_Cogar_Group_N's documentation!
+Assignment_Cogar_Group_N's documentation!
 ====================================================
+
+- AmirMahdi Matin, 5884715
 
 .. toctree::
    :maxdepth: 2
@@ -24,5 +26,9 @@ Assignment: Topic 3
 
 This is the description of the assignment
 
+The architecture
+================
+
 The components
 ================
+
